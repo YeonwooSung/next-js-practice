4f160b2f722dd9e8927fdca2b9ba4ab70c51c8c1
YeonwooSung <neos960518@gmail.com>
2024-02-06 17:47:39 +0900

    feat: Implement the food ordering svc

--- food-ordering/src/app/layout.js
+++ food-ordering/src/app/layout.js
@@ -1,3 +1,5 @@
+import {AppProvider} from "@/components/AppContext";
+import Header from "@/components/layout/Header";
 import { Roboto } from 'next/font/google'
 import './globals.css'
 import {Toaster} from "react-hot-toast";
@@ -14,14 +16,14 @@ export default function RootLayout({ children }) {
     <html lang="en" className="scroll-smooth">
       <body className={roboto.className}>
         <main className="max-w-4xl mx-auto p-4">
-          {/* <AppProvider> */}
+          <AppProvider>
             <Toaster />
-            {/* <Header /> */}
+            <Header />
             {children}
             <footer className="border-t p-8 text-center text-gray-500 mt-16">
               &copy; 2023 All rights reserved
             </footer>
-          {/* </AppProvider> */}
+          </AppProvider>
         </main>
       </body>
     </html>
